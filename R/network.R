2aaca2e2c617af054d129296537f5d0527e21614
sktron <simo.kitanovski@uni-due.de>
2023-03-10 15:44:45 +0100

    doc tested with roxygen, empty scripts for scoring and network drawing added

---
+++ R/network.R
@@ -0,0 +1,5 @@
+# Description:
+# This function will create an igraph or visGraph object. TODO
+plot_graph <- function(gliph_score) {
+
+}
---
+++ R/score.R
@@ -0,0 +1,9 @@
+
+# Description:
+# This function will perform the scoring of the clusters based on
+# e.g. HLA, V, J, clone size TODO
+score <- function(gliph,
+                  data_sample,
+                  data_meta) {
+
+}
