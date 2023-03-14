8ec1d6e1d3f7e3de65f4794c9e949d92d94f516d
sktron <simo.kitanovski@uni-due.de>
2023-03-15 00:05:59 +0100

    quite good checks, one that I would also add is a check if input is missing = not provided by the user

--- R/input_checks.R
+++ R/input_checks.R
@@ -40,12 +40,18 @@ check_data_ref <- function(data_ref) {
 #' @param version, integer: version = 1, 2 or 3, gliph version to use
 #' @noRd
 check_version <- function(version) {
-    if(!base::is.numeric(version))
+    if(base::missing(version)){
+        base::stop("missing input parameter version")
+    }
+    if(!base::is.numeric(version)) {
         base::stop("version has to be numeric")
-    if(base::length(version) > 1)
+    }
+    if(base::length(version)!=1) {
         base::stop("version has to be a single number")
-    if(!(version %in% base::c(1, 2, 3) ))
+    }
+    if(!(version %in% base::c(1, 2, 3))) {
         base::stop("version has to be 1, 2 or 3")
+    }
 }
 
 #' Input checks for motif lengths and local minimum ove
