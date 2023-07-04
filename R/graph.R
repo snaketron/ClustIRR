1d07536a01acd61949ab824e760219b48239a331
Kai Wollek <kai.wollek@stud.uni-duisburg-essen.de>
2023-07-04 20:35:35 +0200

    merge / readded data / minor changes to comply with bioccheck

--- R/graph.R
+++ R/graph.R
@@ -1,6 +1,6 @@
-get_graph <- function(clustirr) {
+get_graph <- function(clust_irr) {
     
-    edges <- get_edges(clustirr)
+    edges <- get_edges(clust_irr)
     
     igraph_object <- igraph::graph_from_data_frame(edges, 
                                                    directed = FALSE)
