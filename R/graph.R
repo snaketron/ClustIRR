d7968a43d84c6abfb8bf588107bb91e33cce8c87
Kai Wollek <kai.wollek@stud.uni-duisburg-essen.de>
2023-07-05 12:55:47 +0200

    Fixed MacOS related R Studio problem
    Problem:
    -> when using "import(utils)" in NAMESPACE, after using devtools::load_all calling View() lead to opening separate XQuartz window
    
    Cause:
    -> presumably caused by utils::View() overwriting internal View() function of R Studio
    
    Fix:
    -> when using "importFrom(utils, comb)" (only used function in package) in NAMESPACE, problem solved

--- R/graph.R
+++ R/graph.R
@@ -2,7 +2,29 @@ get_graph <- function(clust_irr) {
     
     edges <- get_edges(clust_irr)
     
-    igraph_object <- igraph::graph_from_data_frame(edges, 
-                                                   directed = FALSE)
-    return(igraph_object)
+    # if we don't compute this, igraph will break some from/to connections
+    # and effectively make the graph useless by assigning wrong indices
+    v <- unique(c(unique(edges$from), unique(edges$to)))
+    
+    ig <- igraph::graph_from_data_frame(edges, 
+                                        directed = FALSE,
+                                        vertices = v)
+    return(ig)
+}
+
+
+plot_graph <- function(ig) {
+  
+  # prepare nodes & edges for visNetwork
+  nodes <- igraph::as_data_frame(ig, what = "vertices")
+  edges <- igraph::as_data_frame(ig, what = "edges")
+  
+  names(nodes) <- "id"
+  nodes$color.border <- "black"
+  nodes$color.highlight <- "red"
+  nodes$size <- 20
+  
+
+  
+  return(NULL)
 }
