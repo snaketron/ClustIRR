e3fd844a2261b9c532c0cb9bd0c4a32d76125716
sktron <simo.kitanovski@uni-due.de>
2024-07-01 17:10:58 +0200

    make ClustIRR compatible with Bioconductor 3.18 for now, until we get R=4.4 and Bioconductor 3.19 on the VMs

--- R/graph.R
+++ R/graph.R
@@ -601,7 +601,8 @@ get_intergraph_edges_blosum <- function(igs,
     
     # get blosum matrix from pwalign
     data_env <- new.env(parent = emptyenv())
-    data("BLOSUM62", envir = data_env, package = "pwalign")
+    # data("BLOSUM62", envir = data_env, package = "pwalign")
+    data("BLOSUM62", envir = data_env, package = "Biostrings")
     
     # compute BLSOUM62 score for matches
     o$bs <- sapply(X = 1:nrow(o), FUN = get_bscore, d = o, 
