734de22fe28834388005a28bbe07be39020e22aa
snaketron <simo.kitanovski@uni-due.de>
2025-01-07 19:27:08 +0100

    honeycomb implementation

--- R/annotation_utils.R
+++ R/annotation_utils.R
@@ -278,7 +278,6 @@ get_node_ann <- function(node_summary, ag_key) {
     vars <- c()
     
     for(i in 1:nrow(ag_key)) {
-        browser()
         a<- get_ann(ns = node_summary, ag = ag_key$ag[i], type = ag_key$type[i])
         vars <- c(vars, colnames(a))
         m <- cbind(m, a)
@@ -309,12 +308,14 @@ get_beta_violins <- function(beta,
 
         g <- ggplot(data = d)+
             geom_sina(aes(x = sample, y = mean, col = specificity, 
-                          size = size), alpha = 0.75)+
-            theme_bw()+
+                          size = size), stroke = 0.5, alpha = 0.4)+
+            theme_bw(base_size = 10)+
             ggtitle(label = x, subtitle = paste0(db, ", chains: ", chain))+
             scale_radius(name = "Specific cells", 
                          breaks = scales::pretty_breaks(n = 4),
-                         range = c(0.5, 4))
+                         range = c(0.5, 4))+
+            guides(size = guide_legend(order = 2), 
+                   colour = guide_legend(order = 1))
         
         return(g)
     }
@@ -365,9 +366,7 @@ get_beta_violins <- function(beta,
                     data.frame(ag = ag_genes, type = "gene"))
     ag_key <- ag_key[complete.cases(ag_key),]
     
-    browser()
     na <- get_node_ann(node_summary = node_summary, ag_key = ag_key)
-    
     
     d <- na$ann %>% 
         group_by(community, sample) %>% 
@@ -393,4 +392,46 @@ get_beta_violins <- function(beta,
                 vars = vars,
                 violins = v))
 }
+
+
+get_honeycombs <- function(com) {
+    n <- ncol(com)
+    gs <- vector(mode = "list", length = n*(n-1)/2)
+    iter <- 1
+    for(i in 1:(ncol(com)-1)) {
+        for(j in (i+1):ncol(com)) {
+            
+            m <- data.frame(x = com[,i], 
+                            y = com[,j], 
+                            community = 1:nrow(com),
+                            sample_1 = colnames(com)[i], 
+                            sample_2 = colnames(com)[j],
+                            contrast = paste0(colnames(com)[i], "-", colnames(com)[j]),
+                            title = paste0(colnames(com)[i], ' (x) vs ', 
+                                           colnames(com)[j], " (y)"))
+            
+            m$x_adj <- ifelse(test = m$x==0, yes = m$x+0.5, no = m$x)
+            m$y_adj <- ifelse(test = m$y==0, yes = m$y+0.5, no = m$y)
+            
+            gs[[iter]] <- ggplot(data = m)+
+                facet_wrap(facets = ~title)+
+                geom_hex(aes(x = x_adj, y = y_adj, fill=log10(..count..)), 
+                         col = "white", bins = 5)+
+                geom_abline(slope = 1, intercept = 0, linetype = "dashed")+
+                geom_point(aes(x = x_adj, y = y_adj), 
+                           alpha = 0.75, size = 0.5, stroke = 0.3)+
+                scale_x_continuous(name = "x", trans = "log10")+
+                scale_y_continuous(name = "y", trans = "log10")+
+                annotation_logticks(base = 10, sides = "lb")+
+                scale_fill_gradient(low = "white", high = "#FFC68A")+
+                theme(legend.position = "none")
+            
+            iter <- iter + 1
+        }
+    }
+    
+    return(gs)
+}
+
+
     
